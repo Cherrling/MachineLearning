86d08edba57d6bd76aac8b11f2fefb60353d9a9b
cherrling <cherrling2021@outlook.com>
2024-10-13 20:21:19 +0800

    exp2 finish

--- exp2/full.py
+++ exp2/full.py
@@ -1,3 +1,14 @@
+# Import necessary libraries
+from keras.callbacks import TensorBoard
+import os
+import time
+# Set up TensorBoard logging
+log_dir = os.path.join("logs", "fit", "model_{}".format(int(time.time())))
+tensorboard_callback = TensorBoard(log_dir=log_dir, histogram_freq=1)
+
+
+
+
 #1.1
 from keras.datasets import mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
@@ -9,8 +20,7 @@ print('shape of y_test:' + str(y_test.shape))
 #1.2
 x_train_vec_total = x_train.reshape((60000,28,28,1))/255.0
 x_test_vec = x_test.reshape((10000,28,28,1))/255.0
-print('shape of x_train_vec is' + 
-str(x_train_vec_total.shape))
+print('shape of x_train_vec is' + str(x_train_vec_total.shape))
 #
 # #1.3
 import numpy as np
@@ -21,8 +31,7 @@ def to_one_hot(labels,dimension = 10):
     return results
 y_train_vec_total = to_one_hot(y_train)
 y_test_vec = to_one_hot(y_test)
-print('shape of y_train_vec is' + 
-str(y_train_vec_total.shape))
+print('shape of y_train_vec is' + str(y_train_vec_total.shape))
 #
 #
 #
@@ -62,15 +71,15 @@ model.add(layers.Dense(10,activation='softmax')) #Output:10
 #
 # # 3 .1
 from keras import optimizers
-model.compile(optimizers.rmsprop_v2.RMSprop(learning_rate=0.00
-01),
-loss = 'categorical_crossentropy',
-metrics =['accuracy'])
+model.compile(optimizers.rmsprop_v2.RMSprop(learning_rate=0.0001),
+                loss = 'categorical_crossentropy',
+                metrics =['accuracy'])
 #
 # #3.2
-history = model.fit(x_train_vec, y_train_vec,
-batch_size=128, epochs =20,
-validation_data = (x_valid_vec,y_valid_vec))
+history =   model.fit(x_train_vec, y_train_vec,
+            batch_size=128, epochs =200,
+            validation_data = (x_valid_vec,y_valid_vec),
+            callbacks=[tensorboard_callback])  # Add the callback here)
 #
 # 保存模型
 # save model
@@ -87,6 +96,7 @@ model2.summary()
 #print('\n%s : %.2f' % (model2.metrics_names[1], scores[1]*100))
 # #5.1
 #
+"""
 import matplotlib.pyplot as plt
 epochs = range(20) #20 is the number of epochs
 train_acc = history.history['accuracy']
@@ -96,7 +106,10 @@ plt.plot(epochs, valid_acc, 'r', label = 'Validation Accuracy')
 plt.xlabel('Epochs')
 plt.ylabel('Accuracy')
 plt.legend()
+# Save the plot to a file
+plt.savefig('training_validation_accuracy.png')  # Save as PNG file
 plt.show()
+"""
 # #5.2
 #
 loss_and_acc = model.evaluate(x_test_vec, y_test_vec)
